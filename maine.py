5b95abaaaf49d899c1e31459ef347d8f15b7bbb3
Ic-0n0111 <113190401+Ic-0n011@users.noreply.github.com>
2024-05-17 21:54:44 +0300

    Add files via upload

--- maine.py
+++ maine.py
@@ -4,22 +4,23 @@ from random import randint, choice
 Лабиринт
     нечетное колличество (3x3 9x9 5x5 и тд)
 
-    [1][1][1][1][1]
-    [ ][ ][1][ ][1]
-    [1][ ][1][ ][ ]
-    [1][ ][ ][ ][1]
-    [1][1][1][1][1]
+    ⬛⬜🟥🟩🟦
+
+    ⬛⬛⬛⬛⬛
+    ⬜⬜⬛⬜⬛
+    ⬛⬜⬛⬜⬜
+    ⬛⬜⬜⬜⬛
+    ⬛⬛⬛⬛⬛
 
     полностью заполняем стенами -> кидаем бульдозер(начн в четн клетке)
       который делает нам проход(если в двух клетках от него стены - он их ломает)
         заканчивает когда в четных нет стен
           все четные свободны | есть выход
 """
-labyrinth_size = 15
+labyrinth_size = 30
 
 
 # Создаем лабиринт
-
 def create_labyrinth(labyrinth_size):
     if labyrinth_size % 2 == 0:
         labyrinth_size += 1
@@ -31,51 +32,61 @@ def create_labyrinth(labyrinth_size):
     return labyrinth
 
 
+# показ лабиринта
 def show(labyrinth):
     for col in labyrinth:
         cells = []
         for cell in col:
             if cell == 1:
-                cells.append('[+]')
+                cells.append('⬛')
             else:
-                cells.append('[ ]')
+                cells.append('⬜')
         print(*cells)
 
 
-labyrinth = create_labyrinth(labyrinth_size)
-
-
 # делаем в нем проходы
+def drilling_labyrinth() -> None:
+    # создаем кординаты бура, делаем вход и выход
+    drill = [1, 1]
+    labyrinth[drill[0]][drill[1]] = 0
+    labyrinth[0][1] = 0
 
-def drilling_labyrinth(labyrinth, labyrinth_size):
-    drill_cords = [1, 1]
-    drilling = True
-    labyrinth[drill_cords[0]][drill_cords[1]] = 0
-    while drilling:
-        direction = []
-        if 0 < drill_cords[1] - 2:
-            direction.append((0, -1))
-        if drill_cords[1] + 2 < labyrinth_size:
-            direction.append((0, 1))
-        if 0 < drill_cords[0] - 2:
-            direction.append((-1, 0))
-        if drill_cords[0] + 2 < labyrinth_size:
-            direction.append((1, 0))
-
-        if direction and randint(0, 100) != 1: # TODO: ЧЕТНОСТЬ
-            choice_direction = choice(direction)
-            show(labyrinth)
+    labyrinth[labyrinth_size][labyrinth_size-1] = 0
+    crossroads = []
+    while True:
+        clean_passages = []
+        # собираем доступные направления из кординат бура
+        if drill[0] + 2 < labyrinth_size:
+            if labyrinth[drill[0] + 2][drill[1]] == 1:
+                clean_passages.append((1, 0))
+        if drill[1] + 2 < labyrinth_size:
+            if labyrinth[drill[0]][drill[1] + 2]  == 1:
+                clean_passages.append((0, 1))
+        if drill[0] - 2 > 0:
+            if labyrinth[drill[0] - 2][drill[1]] == 1:
+                clean_passages.append((-1, 0))
+        if drill[1] - 2 > 0:
+            if labyrinth[drill[0]][drill[1] - 2] == 1:
+                clean_passages.append((0, -1))
+        
+        # Перекресток - если направлений несколько, то сохраняем эту кординату
+        if clean_passages:
+            crossroads.append([drill[0], drill[1]])
+        
+        if clean_passages:  # если есть доступные направления, бурим
+            direction = choice(clean_passages)
             for _ in range(2):
-                print(drill_cords, choice_direction)
-                drill_cords[0] += choice_direction[0]
-                drill_cords[1] += choice_direction[1]
-                labyrinth[drill_cords[0]][drill_cords[1]] = 0
-        else:
-            drilling = False
+                drill[0] += direction[0]
+                drill[1] += direction[1]
+                labyrinth[drill[0]][drill[1]] = 0
+        elif crossroads:  # иначе перемещаемся на прошлые перекрестки
+            drill = crossroads[-1]
+            crossroads.pop(-1)
+        else:  # если перекрестковне осталось то заканчиваем бурить
             break
-    return labyrinth
+        
 
 
-labyrinth = drilling_labyrinth(labyrinth, labyrinth_size)
-
-
+labyrinth = create_labyrinth(labyrinth_size)
+drilling_labyrinth()
+show(labyrinth)
